7f59aaf1888fe668f16db3f816e9384dc15b7d6b
ZekePlumm44 <iplummer013@gmail.com>
2024-12-23 15:19:31 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <article className="w-full max-w-main grow">
+        <dl className="list-container">
+          <dt className="list-title border-none">
+            <h1>Isaiah Plummer</h1>
+            <h2>Software Engineer</h2>
+          </dt>
+          <dd className="list-content border-none">
+            <p>
+              I'm Isaiah Plummer, a software engineer based out of New York with a
+              passion for creative problem solving, beauty in design, and
+              engineering first-rate software.
+            </p>
+            <p>
+              Previous Software Engineer with General Motors where I helped
+              build web apps used by every dealership in the world.
+            </p>
+            <p>My deep appreciation for the arts, music and literature has given me a unique perspective on software development.</p>
+            <p></p>
+          </dd>
+        </dl >
+        <dl>
+            <dt className="list-title border-none"></dt>
+            <dd className="list-content border-none"></dd>
+        </dl>
+        <dl>
+          <dt className="list-title border-none"></dt>
+          <dd className="list-content border-none"></dd>
+          
+        </dl>
+        <dl>
+          <dt className="list-title border-none"></dt>
+          <dd className="list-content border-none"></dd>
+        </dl>
+      </article>
+      <footer className="w-full max-w-main grow footer-container">
+
+      </footer>
+    </>
+  );
+}
+export default App;
