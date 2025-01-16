31bfe908a608d846797477c5b7c972a62e06fe34
ZekePlumm44 <iplummer013@gmail.com>
2025-01-16 07:14:50 -0500

    Save before travel

--- src/components/home/resume.tsx
+++ src/components/home/resume.tsx
@@ -3,22 +3,31 @@ export default function Resume() {
         {
             year: "2021",
             present: false,
-            role: "Software Engineer",
+            role: "Software Engineer, Customer Care and Aftersales",
             company: "General Motors",
             location: "Atlanta, GA",
-            link: "https://www.gm.com/",
+            link: "https://www.gm.com/"
+        },
+        {
+            year: "2017",
+            present: false,
+            role: "Student, Computer Engineering",
+            company: "Syracuse University",
+            location: "Syracuse, NY",
+            link: "https://www.syracuse.edu/"
         }
     ];
     return (
         <dl className="resume-container">
             {timelineData.map((item, index) => (
-            <div className="list-container" key={index}>
+            <div 
+                className="list-container" key={index}>
                 <dt className="list-title" id="timeline-year">
-                    <h2>{item.year}</h2>
+                    <h3>{item.year}</h3>
                     {item.present && <span className="present-tag">Present</span>}
                 </dt>
                 <dd className="list-content" id="timeline-role">
-                    <h2>{item.role}</h2>
+                    <h3>{item.role}</h3>
                     <a href={item.link} target="_blank" rel="noopener noreferrer">{item.company} ↗</a>
                     <p>{item.location}</p>
                 </dd>
