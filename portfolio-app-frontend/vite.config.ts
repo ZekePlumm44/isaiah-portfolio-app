052163b1e1060dc9a624ce147811bb5938c3edfa
ZekePlumm44 <iplummer013@gmail.com>
2025-02-06 01:09:04 -0500

    Fix docker issues, deploy to render

--- portfolio-app-frontend/vite.config.ts
+++ portfolio-app-frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '/home/zeke/Documents/ProjectsHome/Web_Apps/Portfolio-Web-App/isaiah-portfolio-app/portfolio-app-frontend',
+  root: '.',
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,5 +15,8 @@ export default defineConfig({
         api: 'modern-compiler', // or "modern"
       },
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 });
